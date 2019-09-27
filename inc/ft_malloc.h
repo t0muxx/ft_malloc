f2b15f69874a14c0082afd0a592d26debef3e7ff
tmaraval <maraval.tom@protonmail.com>
2019-09-27 09:11:27 +0200

    double inclusion + new srtuct format

--- inc/ft_malloc.h
+++ inc/ft_malloc.h
@@ -6,44 +6,36 @@
 /*   By: tmaraval <tmaraval@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/24 14:57:13 by tmaraval          #+#    #+#             */
-/*   Updated: 2019/09/25 15:56:39 by tmaraval         ###   ########.fr       */
+/*   Updated: 2019/09/27 09:07:22 by tmaraval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <sys/mman.h>
-#include <stddef.h>
-#include <libft.h>
-#include <errno.h>
+#ifndef FT_MALLOC_H
+# define FT_MALLOC_H
 
-#define ALIGN 16
-//#define SZ_ZONE_TINY 4096*16
-#define SZ_ZONE_TINY 1024*4
-#define SZ_TINY ((SZ_ZONE_TINY/100)-sizeof(struct s_chunk))+(ALIGN-((SZ_ZONE_TINY/100)-sizeof(struct s_chunk))%16)
-#define SZ_ZONE_MEDIUM 4096*32
-#define SZ_MEDIUM ((SZ_ZONE_MEDIUM/100)-sizeof(struct s_chunk))+(ALIGN-((SZ_ZONE_MEDIUM/100)-sizeof(struct s_chunk))%16)
+# include <stdio.h>
+# include <sys/mman.h>
+# include <stddef.h>
+# include <libft.h>
+# include <errno.h>
+# include <unistd.h>
 
-#define DEBUG 1
+# define ALIGN 16
+# define MULTIPLE_ZONE_TINY 16
+# define MULTIPLE_ZONE_MEDIUM 32
+//#define SZ_MEDIUM ((SZ_ZONE_MEDIUM/100)-sizeof(struct s_chunk))+(ALIGN-((SZ_ZONE_MEDIUM/100)-sizeof(struct s_chunk))%16)
+
+# define DEBUG 1
 //#define DEBUG_CHUNK 1
-#define DEBUG_ZONE 1
+# define DEBUG_ZONE 1
 //#define DEBUG_FREE 1
 // Check norm
-#define ALIGN_SIZE(x) (x + ALIGN - (x%ALIGN))
+# define ALIGN_SIZE(x) (x + ALIGN - (x%ALIGN))
 
 enum e_status {
 	USED,
 	FREE
 };
-
-typedef struct	s_zone
-{
-	size_t			size;
-	size_t			used;
-	struct s_zone	*prev;
-	struct s_zone	*next;
-
-}				t_zone;
-
 
 // s_chunk->prev used for alignment
 
@@ -56,17 +48,35 @@ typedef struct	s_chunk
 
 }				t_chunk;
 
+typedef struct	s_zone
+{
+	size_t			size;
+	size_t			used;
+	t_chunk			*chunks;
+	struct s_zone	*next;
+
+}				t_zone;
+
+typedef struct	s_size
+{
+	size_t  sz_zone_tiny;
+	size_t  sz_zone_medium;
+	size_t	sz_tiny;
+	size_t  sz_medium;
+
+}				t_size;
+
 typedef struct	s_malloc
 {
-	t_zone	*zone_tiny;
-	t_chunk *chunk_tiny;
+	t_zone			*zone_tiny;
+	struct s_size	*size;
 
 }				t_malloc;
 
 extern t_malloc g_malloc;
 
 void	*ft_malloc(size_t size);
-int		init_malloc(t_malloc *g_malloc);
+int		init_malloc();
 void	ft_free(void *ptr);
 void	*zone_2_mem(t_zone *zone);
 int		add_zone(t_zone **zone, size_t size);
@@ -75,3 +85,4 @@ void  *search_zone(t_zone **zone, size_t size, size_t sz_zone);
 //DEBUG : 
 void	print_chunks(t_chunk *chunk, char * chunk_name);
 void print_zones(t_zone *zone, char *zone_name);
+#endif
