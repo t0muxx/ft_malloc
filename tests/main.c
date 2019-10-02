809169de73439a3d7dd4ed4ff0e174ce9ec16f47
tmaraval <maraval.tom@protonmail.com>
2019-10-02 15:28:20 +0200

    wip test

--- tests/main.c
+++ tests/main.c
@@ -6,7 +6,7 @@
 /*   By: tmaraval <tmaraval@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/24 16:21:20 by tmaraval          #+#    #+#             */
-/*   Updated: 2019/10/02 14:50:48 by tmaraval         ###   ########.fr       */
+/*   Updated: 2019/10/02 15:08:14 by tmaraval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,15 @@
 int main(void)
 {
 	const struct CMUnitTest tests[] = {
-/*
 		cmocka_unit_test(test_add_zone_null),
         cmocka_unit_test(test_add_zone_after),
         cmocka_unit_test(test_search_zone_same),
         cmocka_unit_test(test_search_zone_needAdd),
         cmocka_unit_test(test_page_free_0pageFree),
-		
         cmocka_unit_test(test_page_free_Page0ToFree),
 		cmocka_unit_test(test_page_free_Page1ToFree),
 		cmocka_unit_test(test_page_free_Page2ToFree),
 		cmocka_unit_test(test_page_free_Page16ToFree),
-
-		*/
 		cmocka_unit_test(test_malloc_simple),
     };
 
