9282c2f274c57cc84672d4a271ec9d029309021e
tmaraval <maraval.tom@protonmail.com>
2019-10-02 09:39:43 +0200

    test on munmap page part ok

--- tests/main.c
+++ tests/main.c
@@ -6,7 +6,7 @@
 /*   By: tmaraval <tmaraval@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/24 16:21:20 by tmaraval          #+#    #+#             */
-/*   Updated: 2019/10/01 19:36:33 by tmaraval         ###   ########.fr       */
+/*   Updated: 2019/10/02 09:26:30 by tmaraval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,17 @@
 int main(void)
 {
 	const struct CMUnitTest tests[] = {
-        cmocka_unit_test(test_add_zone_null),
+/*
+		cmocka_unit_test(test_add_zone_null),
         cmocka_unit_test(test_add_zone_after),
         cmocka_unit_test(test_search_zone_same),
         cmocka_unit_test(test_search_zone_needAdd),
         cmocka_unit_test(test_page_free_0pageFree),
+		*/
         cmocka_unit_test(test_page_free_Page0ToFree),
-        cmocka_unit_test(test_page_free_Page2ToFree),
+		cmocka_unit_test(test_page_free_Page1ToFree),
+		cmocka_unit_test(test_page_free_Page2ToFree),
+		cmocka_unit_test(test_page_free_Page16ToFree),
     };
 
     return cmocka_run_group_tests(tests, NULL, NULL);
--- tests/test_page.c
+++ tests/test_page.c
@@ -6,12 +6,42 @@
 /*   By: tmaraval <tmaraval@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/01 15:24:03 by tmaraval          #+#    #+#             */
-/*   Updated: 2019/10/01 16:46:46 by tmaraval         ###   ########.fr       */
+/*   Updated: 2019/10/02 06:40:25 by tmaraval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_malloc.h"
 #include "tests.h"
+
+void	test_page_free_Page16ToFree(void **state)
+{
+	t_zone *zone;
+	t_chunk *ck;
+	int		i;
+
+	i = 0;
+	ck = NULL;
+	zone = NULL;
+	add_zone(&zone, 16);
+	assert_ptr_not_equal(zone, NULL);
+	assert_ptr_not_equal(zone_2_mem(zone), NULL);
+	zone->chunks = NULL;
+	assert_ptr_equal(zone->chunks, NULL);
+	
+	while (i < 15)
+	{
+		add_chunk(&(zone->chunks), zone_2_mem(zone)+ zone->used, 4064);
+		zone->used += 4064 + sizeof(t_chunk);
+		i++;
+	}
+	add_chunk(&(zone->chunks), zone_2_mem(zone) + zone->used, 4064);
+	zone->chunks->next->next->next->next->next->next->next->next->next->next->next->next->next->next->status = FREE;
+	zone->chunks->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next->status = FREE;
+	print_chunk(zone->chunks);
+	zone->used += 4064 + sizeof(t_chunk);
+	assert_int_equal(cnt_chunks(&(zone->chunks), USED), 14);
+	page_free(&zone, zone->pages_nbr);
+}
 
 void	test_page_free_Page2ToFree(void **state)
 {
@@ -59,7 +89,58 @@ void	test_page_free_Page2ToFree(void **state)
 	page_free(&zone, zone->pages_nbr);
 	assert_int_equal(cnt_chunks(&(zone->chunks), USED), 4);
 	assert_int_equal(cnt_chunks(&(zone->chunks), FREE), 1);
+	assert_int_equal(zone->state[2], FREE);
 
+}
+
+void	test_page_free_Page1ToFree(void **state)
+{
+	t_zone *zone;
+	t_chunk *ck;
+	int i;
+
+	i = 0;
+	ck = NULL;
+	zone = NULL;
+	add_zone(&zone, 16);
+	assert_ptr_not_equal(zone, NULL);
+	assert_ptr_not_equal(zone_2_mem(zone), NULL);
+	zone->chunks = NULL;
+	assert_ptr_equal(zone->chunks, NULL);
+	
+	add_chunk(&(zone->chunks), zone_2_mem(zone), ALIGN_SIZE(3000));
+	zone->used = ALIGN_SIZE(3000) + sizeof(t_chunk);
+	assert_ptr_not_equal(zone->chunks, NULL);
+	
+	add_chunk(&(zone->chunks), zone_2_mem(zone) + zone->used, ALIGN_SIZE(3000));
+	zone->used += ALIGN_SIZE(3000) + sizeof(t_chunk);
+	zone->chunks->next->status = FREE;
+	// FIRST PAGE END
+	
+	add_chunk(&(zone->chunks), zone_2_mem(zone) + zone->used, ALIGN_SIZE(2000));
+	zone->used += ALIGN_SIZE(2000) + sizeof(t_chunk);
+	zone->chunks->next->next->status = FREE;
+
+	add_chunk(&(zone->chunks), zone_2_mem(zone) + zone->used, ALIGN_SIZE(4000));
+	zone->used += ALIGN_SIZE(4000) + sizeof(t_chunk);
+	zone->chunks->next->next->next->status = FREE;
+	// SECOND PAGE	
+	add_chunk(&(zone->chunks), zone_2_mem(zone) + zone->used, ALIGN_SIZE(6000));
+	zone->used += ALIGN_SIZE(6000) + sizeof(t_chunk);
+	zone->chunks->next->next->next->next->status = FREE;
+	
+	add_chunk(&(zone->chunks), zone_2_mem(zone) + zone->used, ALIGN_SIZE(6000));
+	zone->used += ALIGN_SIZE(6000) + sizeof(t_chunk);
+	
+	add_chunk(&(zone->chunks), zone_2_mem(zone) + zone->used, ALIGN_SIZE(6000));
+	zone->used += ALIGN_SIZE(6000) + sizeof(t_chunk);
+	
+	assert_int_equal(cnt_chunks(&(zone->chunks), USED), 3);
+	assert_int_equal(cnt_chunks(&(zone->chunks), FREE), 4);
+	page_free(&zone, zone->pages_nbr);
+	assert_int_equal(cnt_chunks(&(zone->chunks), USED), 3);
+	assert_int_equal(cnt_chunks(&(zone->chunks), FREE), 1);
+	assert_int_equal(zone->state[1], FREE);
 }
 
 void	test_page_free_Page0ToFree(void **state)
