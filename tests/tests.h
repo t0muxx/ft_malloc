d3d6d05b913ad61a3cd502464079e8a50ed93ad1
tmaraval <maraval.tom@protonmail.com>
2019-10-03 18:47:36 +0200

    test on removing munmaping zone

--- tests/tests.h
+++ tests/tests.h
@@ -10,15 +10,17 @@
 #include "ft_malloc.h"
 
 
-void test_add_zone_after(void **state);
-void test_add_zone_null(void **state);
-void test_search_zone_same(void **state);
-void test_search_zone_needAdd(void **state);
-void test_page_free_0pageFree(void **state);
-void test_page_free_Page0ToFree(void **state);
-void test_page_free_Page2ToFree(void **state);
-void test_page_free_Page1ToFree(void **state);
+void	test_add_zone_after(void **state);
+void	test_add_zone_null(void **state);
+void	test_search_zone_same(void **state);
+void	test_search_zone_needAdd(void **state);
+void	test_page_free_0pageFree(void **state);
+void	test_page_free_Page0ToFree(void **state);
+void	test_page_free_Page2ToFree(void **state);
+void	test_page_free_Page1ToFree(void **state);
 void	test_page_free_Page16ToFree(void **state);
-void test_malloc_simple(void **state);
-void test_malloc_multiple(void **state);
-int  cnt_chunks(t_chunk **head, size_t status);
+void	test_malloc_simple(void **state);
+void	test_malloc_multiple(void **state);
+void	test_should_delete_zone(void **state);
+void	test_delete_zone(void **state);
+int		cnt_chunks(t_chunk **head, size_t status);
