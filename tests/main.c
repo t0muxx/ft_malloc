15c8d97529317f9a1e4588a4ca34e17929662051
tmaraval <maraval.tom@protonmail.com>
2019-10-04 18:10:41 +0200

    need to test the new removing zone implem

--- tests/main.c
+++ tests/main.c
@@ -6,7 +6,7 @@
 /*   By: tmaraval <tmaraval@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/24 16:21:20 by tmaraval          #+#    #+#             */
-/*   Updated: 2019/10/04 17:28:28 by tmaraval         ###   ########.fr       */
+/*   Updated: 2019/10/04 18:05:07 by tmaraval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,21 +17,24 @@
 int main(void)
 {
 	const struct CMUnitTest tests[] = {
-        cmocka_unit_test(test_add_zone_large),
+        /*
+		cmocka_unit_test(test_add_zone_large),
 		cmocka_unit_test(test_add_zone_null),
         cmocka_unit_test(test_add_zone_after),
         cmocka_unit_test(test_search_zone_same),
         cmocka_unit_test(test_search_zone_needAdd),
         cmocka_unit_test(test_should_delete_zone),
-        cmocka_unit_test(test_page_free_0pageFree),
         cmocka_unit_test(test_page_free_Page0ToFree),
 		cmocka_unit_test(test_page_free_Page1ToFree),
 		cmocka_unit_test(test_page_free_Page2ToFree),
 		cmocka_unit_test(test_page_free_Page16ToFree),
 		cmocka_unit_test(test_malloc_simple),
 		cmocka_unit_test(test_malloc_multiple),
-        cmocka_unit_test(test_delete_zone),
+        cmocka_unit_test(test_delete_zone),*/
+        cmocka_unit_test(test_page_free_0pageFree),
     };
+	printf("sizeof(t_zone) = %lu\n", sizeof(t_zone));
+	printf("sizeof(t_zone) = %lu\n", ALIGN_SIZE(sizeof(t_zone)));
 	printf("|DEBUG| -> pagesize = %d\n", getpagesize());
     return cmocka_run_group_tests(tests, NULL, NULL);
 }
--- tests/test_zone.c
+++ tests/test_zone.c
@@ -6,7 +6,7 @@
 /*   By: tmaraval <tmaraval@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/27 14:09:07 by tmaraval          #+#    #+#             */
-/*   Updated: 2019/10/04 14:48:45 by tmaraval         ###   ########.fr       */
+/*   Updated: 2019/10/04 17:49:49 by tmaraval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,7 +62,7 @@ void test_search_zone_needAdd(void **state)
 	zone = NULL;
 	add_zone(&zone, 16);
 	assert_ptr_not_equal(zone, NULL);
-	assert_int_equal(zone->size, 4096*16);
+	assert_int_equal(zone->size, 4096*16 - sizeof(t_zone));
 	assert_int_equal(zone->used, 0);
 	zone->used = getpagesize() * 15;
 	ret = search_zone(&zone, getpagesize());
@@ -78,7 +78,7 @@ void test_search_zone_same(void **state)
 	zone = NULL;
 	add_zone(&zone, 16);
 	assert_ptr_not_equal(zone, NULL);
-	assert_int_equal(zone->size, 4096*16);
+	assert_int_equal(zone->size, 4096*16 - sizeof(t_zone));
 	assert_int_equal(zone->used, 0);
 	assert_ptr_equal(search_zone(&zone, 32), zone);
 }
@@ -104,12 +104,12 @@ void test_add_zone_after(void **state)
 	zone = NULL;
 	add_zone(&zone, 16);
 	assert_ptr_not_equal(zone, NULL);
-	assert_int_equal(zone->size, 4096*16);
+	assert_int_equal(zone->size, 4096*16 - sizeof(t_zone));
 	assert_int_equal(zone->used, 0);
 	assert_int_equal(zone->pages_nbr, 16);
 	assert_ptr_equal(zone->chunks, NULL);
 	assert_int_equal(add_zone(&zone,16), 0);
-	assert_int_equal(zone->next->size, 4096*16);
+	assert_int_equal(zone->next->size, 4096*16 - sizeof(t_zone));
 	assert_int_equal(zone->next->used, 0);
 	assert_int_equal(zone->next->pages_nbr, 16);
 	assert_ptr_equal(zone->next->chunks, NULL);
@@ -122,7 +122,7 @@ void test_add_zone_null(void **state)
 	zone = NULL;
 	assert_int_equal(add_zone(&zone,16), 0);
 	assert_ptr_not_equal(zone, NULL);
-	assert_int_equal(zone->size, 4096*16);
+	assert_int_equal(zone->size, 4096*16 - sizeof(t_zone));
 	assert_int_equal(zone->used, 0);
 	assert_int_equal(zone->pages_nbr, 16);
 	assert_ptr_equal(zone->chunks, NULL);
