aba3e80ff205d18853f5ff8053b2a1b96a32a822
tmaraval <maraval.tom@protonmail.com>
2019-10-10 16:18:29 +0200

    bit cleaner

--- src/page.c
+++ src/page.c
@@ -6,19 +6,71 @@
 /*   By: tmaraval <tmaraval@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/01 15:07:40 by tmaraval          #+#    #+#             */
-/*   Updated: 2019/10/10 15:44:36 by tmaraval         ###   ########.fr       */
+/*   Updated: 2019/10/10 16:18:19 by tmaraval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_malloc.h"
+
+int		page_free_should_remove_page(t_zone **current_zone,
+		t_chunk **chunk_check, t_chunk **chunk_keep, int p)
+{
+	int		state;
+	int		i;
+
+	state = 0;
+	i = 0;
+#ifdef DEBUG_PAGE
+	ft_putstr("page = ");
+	ft_putnbr(p);
+	ft_putendl("");
+#endif
+	if (p > 0 && *chunk_check && (*chunk_check)->prev 
+	&& (void *)*chunk_check != (void *)(*current_zone) + getpagesize() * (p))
+		*chunk_check = (*chunk_check)->prev;
+	*chunk_keep = *chunk_check;
+	while (*chunk_check && (void *)*chunk_check < (void *)(*current_zone) + getpagesize() * (p + 1))
+	{
+#ifdef DEBUG_PAGE
+		ft_putendl("chunk_check : ");
+		print_chunk(*chunk_check);
+#endif
+		if ((*chunk_check)->status == TO_REMOVE)
+			state += 1;
+		else
+			state += (*chunk_check)->status;
+		i++;
+		*chunk_check = (*chunk_check)->next;
+	}
+	if (state == i && state != 0)
+		return (1);
+	return (0);
+}
+
+void	page_free_update_chunk_status(t_zone **current_zone,
+		t_chunk **chunk_keep, int p)
+{
+	if (p > 0)
+	{
+		while (*chunk_keep && (void *)*chunk_keep
+		< (void *)(*current_zone) + getpagesize() * (p + 1))
+		{
+#ifdef DEBUG_PAGE
+			ft_putendl("added chunk : ");
+			print_chunk(*chunk_keep);
+#endif
+			(*chunk_keep)->status = TO_REMOVE;	
+			*chunk_keep = (*chunk_keep)->next;
+		}
+	(*current_zone)->state[p] = TO_FREE;
+	}
+}
 
 void	page_free_fill_chunk_to_free(t_zone **current_zone)
 {
 	int		p;
 	t_chunk *chunk_check;
 	t_chunk *chunk_keep;
-	int		state;
-	int		i;
 
 	p = 0;
 	chunk_check = (*current_zone)->chunks;
@@ -29,50 +81,14 @@ void	page_free_fill_chunk_to_free(t_zone **current_zone)
 			> (void *)*current_zone + getpagesize() * (p + 1)
 			&& (*current_zone)->state[p] != FREE)
 		{
-			state = 0;
-			i = 0;
-#ifdef DEBUG_PAGE
-			ft_putstr("page = ");
-			ft_putnbr(p);
-			ft_putendl("");
-#endif
-			if (p > 0 && chunk_check && chunk_check->prev && chunk_check != (void *)(*current_zone) + getpagesize() * (p))
-				chunk_check = chunk_check->prev;
-			chunk_keep = chunk_check;
-			while (chunk_check && (void *)chunk_check < (void *)(*current_zone) + getpagesize() * (p + 1))
-			{
-#ifdef DEBUG_PAGE
-				ft_putendl("chunk_check : ");
-				print_chunk(chunk_check);
-#endif
-				if (chunk_check->status == TO_REMOVE)
-					state += 1;
-				else
-					state += chunk_check->status;
-				i++;
-				chunk_check = chunk_check->next;
-			}
 #ifdef DEBUG_PAGE
 			ft_putstr("chunk_keep = ");
 			ft_putptr(chunk_keep);
 			ft_putendl("");
 #endif
-			if (state == i && state != 0)
-			{
-				if (p > 0)
-				{
-					while (chunk_keep && (void *)chunk_keep < (void *)(*current_zone) + getpagesize() * (p + 1))
-					{
-#ifdef DEBUG_PAGE
-						ft_putendl("added chunk : ");
-						print_chunk(chunk_keep);
-#endif
-						chunk_keep->status = TO_REMOVE;	
-						chunk_keep = chunk_keep->next;
-					}
-					(*current_zone)->state[p] = TO_FREE;
-				}
-			}
+			if (page_free_should_remove_page(current_zone,
+				&chunk_check, &chunk_keep, p) == 1)
+					page_free_update_chunk_status(current_zone, &chunk_keep, p);
 		}
 		p++;
 	}
