558cb877a71c2f110e8798cf97ebd980a11d0fcd
Tom MARAVAL <tmaraval@e1r13p4.42.fr>
2019-10-17 10:15:08 +0200

    same

--- src/debug.c
+++ src/debug.c
@@ -6,15 +6,17 @@
 /*   By: tmaraval <tmaraval@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/25 10:51:30 by tmaraval          #+#    #+#             */
-/*   Updated: 2019/10/16 13:03:46 by tmaraval         ###   ########.fr       */
+/*   Updated: 2019/10/17 10:12:04 by tmaraval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_malloc.h"
 
+extern t_malloc g_malloc_state;
+
 void	debug_munmap(void *addr, size_t size)
 {
-	if (getenv("DEBUG_MUNMAP"))
+	if (g_malloc_state.opt & DEBUG_MUNMAP)
 	{
 		ft_putstr("|DEBUG| -> munmap(");
 		ft_putptr(addr);
