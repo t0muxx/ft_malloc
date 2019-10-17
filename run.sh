3de611c7a92b915641094580b96dc492a1fb20cd
Tom MARAVAL <tmaraval@e1r13p4.42.fr>
2019-10-17 12:32:04 +0200

    run.sh

--- run.sh
+++ run.sh
@@ -19,15 +19,15 @@ else
 	export DYLD_LIBRARY_PATH=.
 	export DYLD_INSERT_LIBRARIES="libft_malloc.so"
 	export DYLD_FORCE_FLAT_NAMESPACE=1
-	export DEBUG_FREE=1
-	export DEBUG_FREE_MEM=1
-	export DEBUG_MALLOC=1
-	export DEBUG_MALLOC_RET=1
-	export DEBUG_CALLOC=1
-	export DEBUG_CALLOC_RET=1
-	export DEBUG_REALLOC=1
-	export DEBUG_REALLOC_RET=1
-	export DEBUG_MMAP=1
-	export DEBUG_MUNMAP=1
+#	export DEBUG_FREE=1
+#	export DEBUG_FREE_MEM=1
+#	export DEBUG_MALLOC=1
+#	export DEBUG_MALLOC_RET=1
+#	export DEBUG_CALLOC=1
+#	export DEBUG_CALLOC_RET=1
+#	export DEBUG_REALLOC=1
+#	export DEBUG_REALLOC_RET=1
+#	export DEBUG_MMAP=1
+#	export DEBUG_MUNMAP=1
 	$@
 fi
