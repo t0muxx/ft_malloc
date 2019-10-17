7e2943706399e6c47429cea75d8d15b9bc920dfa
Tom MARAVAL <tmaraval@e1r13p4.42.fr>
2019-10-17 12:01:36 +0200

    norme

--- src/page.c
+++ src/page.c
@@ -6,7 +6,7 @@
 /*   By: tmaraval <tmaraval@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/01 15:07:40 by tmaraval          #+#    #+#             */
-/*   Updated: 2019/10/16 12:59:25 by tmaraval         ###   ########.fr       */
+/*   Updated: 2019/10/17 12:01:26 by tmaraval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,12 @@ int		page_free_should_remove_page(t_zone **current_zone,
 
 	state = 0;
 	i = 0;
-	if (p > 0 && *chunk_check && (*chunk_check)->prev 
+	if (p > 0 && *chunk_check && (*chunk_check)->prev
 	&& (void *)*chunk_check != (void *)(*current_zone) + getpagesize() * (p))
 		*chunk_check = (*chunk_check)->prev;
 	*chunk_keep = *chunk_check;
-	while (*chunk_check && (void *)*chunk_check < (void *)(*current_zone) + getpagesize() * (p + 1))
+	while (*chunk_check && (void *)*chunk_check
+			< (void *)(*current_zone) + getpagesize() * (p + 1))
 	{
 		if ((*chunk_check)->status == TO_REMOVE)
 			state += 1;
@@ -46,10 +47,10 @@ void	page_free_update_chunk_status(t_zone **current_zone,
 		while (*chunk_keep && (void *)*chunk_keep
 		< (void *)(*current_zone) + getpagesize() * (p + 1))
 		{
-			(*chunk_keep)->status = TO_REMOVE;	
+			(*chunk_keep)->status = TO_REMOVE;
 			*chunk_keep = (*chunk_keep)->next;
 		}
-	(*current_zone)->state[p] = TO_FREE;
+		(*current_zone)->state[p] = TO_FREE;
 	}
 }
 
@@ -68,13 +69,12 @@ void	page_free_fill_chunk_to_free(t_zone **current_zone)
 			> (void *)*current_zone + getpagesize() * (p + 1)
 			&& (*current_zone)->state[p] != FREE)
 		{
-			if (page_free_should_remove_page(current_zone,
-				&chunk_check, &chunk_keep, p) == 1)
-					page_free_update_chunk_status(current_zone, &chunk_keep, p);
+			if (page_free_should_remove_page(
+					current_zone, &chunk_check, &chunk_keep, p) == 1)
+				page_free_update_chunk_status(current_zone, &chunk_keep, p);
 		}
 		p++;
 	}
-
 }
 
 void	page_free_remove_chunks(t_zone **current_zone)
@@ -86,7 +86,7 @@ void	page_free_remove_chunks(t_zone **current_zone)
 	cpy = NULL;
 	while (chunk_to_free)
 	{
-		cpy =  chunk_to_free;
+		cpy = chunk_to_free;
 		if (chunk_to_free->status == TO_REMOVE)
 			delete_chunk(&((*current_zone)->chunks), chunk_to_free);
 		chunk_to_free = cpy->next;
