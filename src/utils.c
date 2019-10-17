aefeed67e017db55a0b2ab47e49b6453050d0dfc
Tom MARAVAL <tmaraval@e1r13p4.42.fr>
2019-10-17 12:11:14 +0200

    norme

--- src/utils.c
+++ src/utils.c
@@ -6,37 +6,11 @@
 /*   By: tmaraval <tmaraval@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/25 14:51:56 by tmaraval          #+#    #+#             */
-/*   Updated: 2019/10/17 10:42:06 by tmaraval         ###   ########.fr       */
+/*   Updated: 2019/10/17 12:10:51 by tmaraval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_malloc.h"
-
-void	init_opt(unsigned long *opt)
-{
-	if (!(*opt & OPT_SET))
-		*opt |= 1UL << 10;
-	if (getenv("DEBUG_FREE"))
-		*opt |= 1UL << 0;
-	if (getenv("DEBUG_FREE_MEM"))
-		*opt |= 1UL << 1;
-	if (getenv("DEBUG_MALLOC"))
-		*opt |= 1UL << 2;
-	if (getenv("DEBUG_MALLOC_RET"))
-		*opt |= 1UL << 3;
-	if (getenv("DEBUG_CALLOC"))
-		*opt |= 1UL << 4;
-	if (getenv("DEBUG_CALLOC_RET"))
-		*opt |= 1UL << 5;
-	if (getenv("DEBUG_REALLOC"))
-		*opt |= 1UL << 6;
-	if (getenv("DEBUG_REALLOC_RET"))
-		*opt |= 1UL << 7;
-	if (getenv("DEBUG_MUNMAP"))
-		*opt |= 1UL << 8;
-	if (getenv("DEBUG_MMAP"))
-		*opt |= 1UL << 9;
-}
 
 size_t	aligne_large(size_t size)
 {
