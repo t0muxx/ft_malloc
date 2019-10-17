376d298cba8da02e5e60346c888a220194281490
Tom MARAVAL <tmaraval@e1r13p4.42.fr>
2019-10-17 11:58:30 +0200

    norme

--- inc/ft_malloc.h
+++ inc/ft_malloc.h
@@ -6,7 +6,7 @@
 /*   By: tmaraval <tmaraval@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/24 14:57:13 by tmaraval          #+#    #+#             */
-/*   Updated: 2019/10/17 10:41:16 by tmaraval         ###   ########.fr       */
+/*   Updated: 2019/10/17 11:58:20 by tmaraval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,22 +22,22 @@
 # include <pthread.h>
 
 # define ALIGN 16
-# define MULTIPLE_ZONE_TINY 16 
+# define MULTIPLE_ZONE_TINY 16
 # define MULTIPLE_ZONE_MEDIUM 32
 
 /*
-* ENV debug variable :
-* DEBUG_FREE : will print all call to free();
-* DEBUG_FREE_MEM : will print chunk mem before free
-* DEBUG_MALLOC : will print all call to malloc();
-* DEBUG_MALLOC_RET : will print return value from malloc();
-* DEBUG_CALLOC : will print all call to calloc();
-* DEBUG_CALLOC_RET : will print return value from calloc();
-* DEBUG_REALLOC : will print all call to realloc();
-* DEBUG_REALLOC_RET : will print return value from realloc();
-* DEBUG_MUNMAP : will print each call to munmap();
-* DEBUG_MMAP : will print each call to mmap();
-*/ 
+** ENV debug variable :
+** DEBUG_FREE : will print all call to free();
+** DEBUG_FREE_MEM : will print chunk mem before free
+** DEBUG_MALLOC : will print all call to malloc();
+** DEBUG_MALLOC_RET : will print return value from malloc();
+** DEBUG_CALLOC : will print all call to calloc();
+** DEBUG_CALLOC_RET : will print return value from calloc();
+** DEBUG_REALLOC : will print all call to realloc();
+** DEBUG_REALLOC_RET : will print return value from realloc();
+** DEBUG_MUNMAP : will print each call to munmap();
+** DEBUG_MMAP : will print each call to mmap();
+*/
 
 # define DEBUG_FREE 1
 # define DEBUG_FREE_MEM 2
@@ -91,54 +91,54 @@ typedef struct	s_malloc
 }				t_malloc;
 
 /*
-** Front fucntion 
+** Front fucntion
 */
-void	*ft_malloc(size_t size);
-void	ft_free(void *ptr);
-void	*ft_realloc(void *ptr, size_t size);
-void	*realloc_large(void *ptr, size_t size);
-void	*ft_calloc(size_t nmemb, size_t size);
-void	show_alloc_mem();
+void			*ft_malloc(size_t size);
+void			ft_free(void *ptr);
+void			*ft_realloc(void *ptr, size_t size);
+void			*realloc_large(void *ptr, size_t size);
+void			*ft_calloc(size_t nmemb, size_t size);
+void			show_alloc_mem();
 
 /*
 ** ZONE :
 */
-void	*zone_2_mem(t_zone *zone);
-t_zone	*mem_2_zone(void *mem);
-int		add_zone(t_zone **zone, size_t pages_nbr);
-t_zone	*search_zone(t_zone **zone, size_t size);
-int		zone_need_free(t_zone **zone, size_t size_alloc_min);
-void	remove_zone(t_zone **zone, size_t size_alloc_min);
-int		cnt_zone(t_zone *zone);
-int		should_delete_zone(t_zone *zone);
-void	delete_zone(t_zone **zone, t_zone *del, size_t size);
-t_zone	*add_zone_large(t_zone **zone, size_t size);
-void	shrink_zone(t_zone **zone);
+void			*zone_2_mem(t_zone *zone);
+t_zone			*mem_2_zone(void *mem);
+int				add_zone(t_zone **zone, size_t pages_nbr);
+t_zone			*search_zone(t_zone **zone, size_t size);
+int				zone_need_free(t_zone **zone, size_t size_alloc_min);
+void			remove_zone(t_zone **zone, size_t size_alloc_min);
+int				cnt_zone(t_zone *zone);
+int				should_delete_zone(t_zone *zone);
+void			delete_zone(t_zone **zone, t_zone *del, size_t size);
+t_zone			*add_zone_large(t_zone **zone, size_t size);
+void			shrink_zone(t_zone **zone);
 
-
-void	init_opt(unsigned long *opt);
-size_t	aligne_large(size_t size);
-size_t	size_max(size_t multiple);
+void			init_opt(unsigned long *opt);
+size_t			aligne_large(size_t size);
+size_t			size_max(size_t multiple);
 
 /*
 ** PAGE :
 */
-void	page_free(t_zone **current_zone);
+void			page_free(t_zone **current_zone);
 
-/* 
-** CHUNKS :  
+/*
+** CHUNKS :
 */
-int		have_free_chunk(t_chunk **head);
-int		search_chunk(t_zone **zone, void *ptr);
-void	remove_free_chunk(t_chunk **head);
-void	munmap_small_medium(t_zone **zones);
-int		have_free_chunk(t_chunk **head);
-void	add_chunk(t_chunk **head, void *zone_base, size_t sz_aligned);
-void	delete_chunk(t_chunk **head, t_chunk *toDelete);
-void 	*search_free_chunk(t_zone *head, size_t sz_aligned);
-int		search_chunk_large(t_zone **zones, void *ptr);
+int				have_free_chunk(t_chunk **head);
+int				search_chunk(t_zone **zone, void *ptr);
+void			remove_free_chunk(t_chunk **head);
+void			munmap_small_medium(t_zone **zones);
+int				have_free_chunk(t_chunk **head);
+void			add_chunk(t_chunk **head, void *zone_base, size_t sz_aligned);
+void			delete_chunk(t_chunk **head, t_chunk *to_delete);
+void			*search_free_chunk(t_zone *head, size_t sz_aligned);
+int				search_chunk_large(t_zone **zones, void *ptr);
 
-
-//DEBUG : 
-void	debug_munmap(void *addr, size_t size);
+/*
+** DEBUG :
+*/
+void			debug_munmap(void *addr, size_t size);
 #endif
